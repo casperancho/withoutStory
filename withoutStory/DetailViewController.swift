45134fc95b5fda0ec44bb5b26241e78de2aa84c3
casperancho <artemzakirov1998@gmail.com>
2019-02-04 21:43:46 +0300

    now giving info from vc

--- withoutStory/DetailViewController.swift
+++ withoutStory/DetailViewController.swift
@@ -10,9 +10,26 @@ import Foundation
 import UIKit
 
 class DetailViewController : UIViewController{
+    var forlabel = ""
     override func viewDidLoad() {
         super .viewDidLoad()
         view.backgroundColor =  .purple
+        var infoLabel = UILabel()
+        print(forlabel)
+        infoLabel.text = forlabel
+        view.addSubview(infoLabel)
+        infoLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        let horizontal = NSLayoutConstraint(item: infoLabel, attribute: .centerX, relatedBy: .equal, toItem: view, attribute: .centerX, multiplier: 1.0, constant: 0)
+        let vert = NSLayoutConstraint(item: infoLabel, attribute: .centerY, relatedBy: .equal, toItem: view, attribute: .centerY, multiplier: 1.0, constant: 0)
+        let width = NSLayoutConstraint(item: infoLabel, attribute: .width, relatedBy: .equal, toItem: nil, attribute: .width, multiplier: 1.0, constant: 75)
+        let height = NSLayoutConstraint(item: infoLabel, attribute: .height, relatedBy: .equal, toItem: nil, attribute: .height, multiplier: 1.0, constant: 30)
+        
+        let constrains: [NSLayoutConstraint] = [horizontal, vert,width,height]
+        NSLayoutConstraint.activate(constrains)
     }
+    
+    
+    
 }
 
--- withoutStory/MainViewController.swift
+++ withoutStory/MainViewController.swift
@@ -10,6 +10,7 @@ import Foundation
 import UIKit
 
 class MainViewController : UIViewController{
+    let textEdi = UITextField()
     override func viewDidLoad() {
         super .viewDidLoad()
         view.backgroundColor =  .blue
@@ -17,31 +18,42 @@ class MainViewController : UIViewController{
     }
     
     @objc func buttonTapped(){
-        print("tappeed")
+        print(textEdi.text)
         let DetailVC = DetailViewController()
+        DetailVC.forlabel = textEdi.text!
         navigationController?.pushViewController(DetailVC, animated: true)
     }
 
     private func addButton(){
         let mainButton = UIButton()
+        
+        textEdi.backgroundColor = .white
+        textEdi.translatesAutoresizingMaskIntoConstraints = false
         mainButton.translatesAutoresizingMaskIntoConstraints = false
         mainButton.addTarget(self, action: #selector(buttonTapped),for: .touchUpInside)
         mainButton.setTitle("Go to Detail", for: .normal)
         mainButton.setTitleColor(.white, for: .normal)
         mainButton.backgroundColor = .green
-        
+       
         view.addSubview(mainButton)
+        view.addSubview(textEdi)
         
         let horizontalCenter = NSLayoutConstraint(item: mainButton, attribute: .centerX, relatedBy: .equal, toItem: view, attribute: .centerX, multiplier: 1.0, constant: 0)
-        
         let verticalCenter = NSLayoutConstraint(item: mainButton, attribute: .centerY, relatedBy: .equal, toItem: view, attribute: .centerY, multiplier: 1.0, constant: 0)
-        
+
         let width = NSLayoutConstraint(item: mainButton, attribute: .width, relatedBy: .equal, toItem: nil, attribute: .width, multiplier: 1.0, constant: 100)
-        
+
         let height = NSLayoutConstraint(item: mainButton, attribute: .height, relatedBy: .equal, toItem: nil, attribute: .height, multiplier: 1.0, constant: 75)
         
-        let constraints: [NSLayoutConstraint] = [horizontalCenter,verticalCenter, width, height]
+        
+        let thorizontalCenter = NSLayoutConstraint(item: textEdi, attribute: .centerX, relatedBy: .equal, toItem: view, attribute: .centerX, multiplier: 1.0, constant: 0)
+        let tverticalCenter = NSLayoutConstraint(item: textEdi, attribute: .centerY, relatedBy: .equal, toItem: mainButton, attribute: .centerY, multiplier: 1.0, constant: 80)
+        let twidth = NSLayoutConstraint(item: textEdi, attribute: .width, relatedBy: .equal, toItem: nil, attribute: .width, multiplier: 1.0, constant: 100)
+        let theight = NSLayoutConstraint(item: textEdi, attribute: .height, relatedBy: .equal, toItem: nil, attribute: .height, multiplier: 1.0, constant: 30)
+        
+        let constraints: [NSLayoutConstraint] = [horizontalCenter,verticalCenter, width, height, thorizontalCenter,tverticalCenter, twidth, theight]
         NSLayoutConstraint.activate(constraints)
+        
     }
     
 }
