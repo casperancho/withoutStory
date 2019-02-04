62907bff3e1e22f26ff488e23a715b5c21844126
casperancho <artemzakirov1998@gmail.com>
2019-02-04 19:41:13 +0300

    easy app w tabBar n navcontroller n 2views

---
+++ withoutStory/DetailViewController.swift
@@ -0,0 +1,18 @@
+//
+//  DetailViewController.swift
+//  withoutStory
+//
+//  Created by Артем Закиров on 04.02.2019.
+//  Copyright © 2019 bmstu. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class DetailViewController : UIViewController{
+    override func viewDidLoad() {
+        super .viewDidLoad()
+        view.backgroundColor =  .purple
+    }
+}
+
---
+++ withoutStory/SecondaryViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SecondaryViewController.swift
+//  withoutStory
+//
+//  Created by Артем Закиров on 04.02.2019.
+//  Copyright © 2019 bmstu. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class SecondaryViewController : UIViewController{
+    override func viewDidLoad() {
+        super .viewDidLoad()
+        view.backgroundColor =  .red
+    }
+}
